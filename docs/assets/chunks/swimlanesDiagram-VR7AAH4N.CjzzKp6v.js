804df2b7493be8be530b4a1d2791c9618a1385ba
outcomer <outcomer@users.noreply.github.com>
2026-09-02 13:10:23 +0000

    deploy: 5369970be4df730cab2e65fcb79cc2274414d91d

---
+++ docs/assets/chunks/swimlanesDiagram-VR7AAH4N.CjzzKp6v.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-HODETNUW.0mCTIOEk.js";import{_ as a}from"../app.Bdiwr4mR.js";import"./chunk-5VM5RSS4.CqFX4Ygc.js";import"./chunk-XXDRQBXY.aNCr3WfS.js";import"./chunk-POPQ4Y6H.DlXHgZ_0.js";import"./chunk-F27PBJKO.B6MoVqQo.js";import"./channel.Ca5EzuLR.js";import"./framework.C0xC9L3O.js";import"./theme.C6cLhGaj.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
